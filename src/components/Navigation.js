543afee32fa254e917a30a4ffb409e73c13095ad
jim4067 <jimmyimpulse2@gmail.com>
2020-11-16 00:36:47 +0300

    feat: dark mode persists even when the page is refreshed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { createGlobalStyle, keyframes } from 'styled-components';
 
 //the global styles for setting the theme
@@ -13,11 +13,11 @@ const GlobalTheme = createGlobalStyle`
 const Fadein = keyframes`
     0%{
         opacity: 0;
-        transform: translate(0, -20px);
+        transform: translate(0, -10px);
     }
     100%{
         opacity: 1;
-        transform: translate(0);
+        transfosrm: translate(0);
     }
 `
 
@@ -28,7 +28,7 @@ const Wrapper = styled.div`
     justify-content: space-between;
     /* margin-top: 8vh; */
     margin: 8vh 0;
-    animation: ${Fadein} ease-in 1s;
+    animation: ${Fadein} ease-in .3s;
 `
 
 //the logo styled
@@ -41,9 +41,9 @@ const ThemeSwitcher = styled.button`
     /* background-color: ${({ dark }) => dark ? "white" : "white"}; */
     background: none;
     /* border: 1px solid #48dbfb; */
-    border: ${({dark}) => dark ? "1px solid #48dbfb": "none" };
+    border: ${({ dark }) => dark ? "1px solid #48dbfb" : "none"};
     border-radius: 4px;
-    box-shadow: ${({dark}) => dark ? "0px 5px 5px -5px darkslategrey": "0px 7px 15px -5px  #00000070"};
+    box-shadow: ${({ dark }) => dark ? "0px 5px 5px -5px darkslategrey" : "0px 7px 15px -5px  #00000070"};
     color: ${({ dark }) => dark ? "white" : "black"};
     height: 2.2em;
     margin-right: 4vw;
@@ -61,9 +61,23 @@ const ThemeSwitcher = styled.button`
 const Navigation = () => {
     const [dark, setDark] = useState(false);
 
+    useEffect(() => {
+        const theme = window.localStorage.getItem('theme');
+        if (theme) {
+            setDark(theme);
+        }
+    }, []);
 
+
+    //make sure you useEffect on this so that the theme is not lost when the window is reloaded
+    //set the value of dark mode in local storage and useEffect tp set the dark variable
     const handleThemeSwitch = () => {
         setDark(!dark);
+
+        //persisting the users theme
+        window.localStorage.setItem(
+            'theme', dark
+        );
     }
 
 
