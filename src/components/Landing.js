6943da963e73a765d4f6522e5b3b90cab1ed3e00
jim4067 <jimmyimpulse2@gmail.com>
2020-11-16 00:23:26 +0300

    feat: the component to house the landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Landing = () => {
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+//make sure that you have the text appering from below and from sideways
