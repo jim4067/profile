667c1763ace3e77e8e34bf4b9d20ee04d59032ff
jim4067 <jimmyimpulse2@gmail.com>
2020-11-16 00:13:32 +0300

    feat: made the navigation for switching theme and displaying the logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Navigation from './components/Navigation';
+import './styles/App.css';
 
-function App() {
 
+const App = () => {
 
 	return (
-		<div>
-			<h1>this is the heading one</h1>
+		<div className='app-container'>
+
+			<Navigation />
+
+			<p> This is the text in the app component</p>
 		</div>
 	);
 }
 
 export default App;
+
+
+//hi there, i am jimmy a self taught and introverted full stack web dev
+
+//add section with the certifications from freecodecamp and fullstackopen site
+
+//in the projects sections add a page that shows all the projects built from the fullstackopen
+//challenge
---
+++ src/components/Navigation.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
+
+//the global styles for setting the theme
+const GlobalTheme = createGlobalStyle`
+ body {
+     color: ${({ dark }) => dark ? "#eceff4" : "#4c566a"};
+     background-color: ${({ dark }) => dark ? "#242933" : "#eceff4"};
+ }
+`
+
+//animations should be out here
+const Fadein = keyframes`
+    0%{
+        opacity: 0;
+        transform: translate(0, -20px);
+    }
+    100%{
+        opacity: 1;
+        transform: translate(0);
+    }
+`
+
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    /* margin-top: 8vh; */
+    margin: 8vh 0;
+    animation: ${Fadein} ease-in 1s;
+`
+
+//the logo styled
+const Logo = styled.h2`
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+//theme changing button
+const ThemeSwitcher = styled.button`
+    /* background-color: ${({ dark }) => dark ? "white" : "white"}; */
+    background: none;
+    /* border: 1px solid #48dbfb; */
+    border: ${({dark}) => dark ? "1px solid #48dbfb": "none" };
+    border-radius: 4px;
+    box-shadow: ${({dark}) => dark ? "0px 5px 5px -5px darkslategrey": "0px 7px 15px -5px  #00000070"};
+    color: ${({ dark }) => dark ? "white" : "black"};
+    height: 2.2em;
+    margin-right: 4vw;
+    outline: none;
+    padding: 0.25em 2em;
+    transition: .4s linear;
+
+    :hover{
+        background-color: #48dbfb;
+        cursor: pointer;
+        color: black;
+    }
+`;
+
+const Navigation = () => {
+    const [dark, setDark] = useState(false);
+
+
+    const handleThemeSwitch = () => {
+        setDark(!dark);
+    }
+
+
+    return (
+        <Wrapper>
+
+            <GlobalTheme dark={dark} />
+
+            <Logo>JIM <span style={{ color: '#48dbfb' }} >.</span>  </Logo>
+
+            <ThemeSwitcher onClick={handleThemeSwitch} dark={dark} >{dark ? "sunrise" : "sunset"}</ThemeSwitcher>
+
+        </Wrapper>
+    );
+}
+
+export default Navigation;
+
+//when switching between dark and light. make the button rounded and ask dark or light
+
+//MAKE SURE you mark the time for transition when doing animations for components fading in and out
+
+//save the them in the local storage and then useEffect hook to see the value stored in local storage to change
+//the theme
+
+//add a oh snap to make the whole page fade away like in avengers
