77bcbd756482f055b45ee18b62ea25142bac42cd
jim4067 <jimmyimpulse2@gmail.com>
2020-11-16 23:55:39 +0300

    moved the state up and moved the global styled here too

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Landing from './components/Landing';
 import Navigation from './components/Navigation';
 import './styles/App.css';
 
 
+//the global styles for setting the theme
+const GlobalTheme = createGlobalStyle`
+ body {
+     background-color: ${({ dark }) => dark ? "#242933" : "#eceff4"};
+     color: ${({ dark }) => dark ? "#eceff4" : "#4c566a"};
+ }
+`
+
+
 const App = () => {
+	const [dark, setDark] = useState(false);
+
+	useEffect(() => {
+		const theme = window.localStorage.getItem('theme');
+
+		if (theme === 'true') {
+			setDark(false);
+		}
+		else {
+			setDark(true);
+		}
+	}, []);
+
+	//make sure you useEffect on this so that the theme is not lost when the window is reloaded
+	//set the value of dark mode in local storage and useEffect tp set the dark variable
+	const handleThemeSwitch = () => {
+		//persisting the users theme
+		window.localStorage.setItem(
+			'theme', dark
+		);
+
+		setDark(!dark);
+	}
+
 
 	return (
 		<div className='app-container'>
 
-			<Navigation />
+			<GlobalTheme dark={dark} />
+
+			<Navigation dark={dark} handleThemeSwitch={handleThemeSwitch} />
+
+			<Landing dark={dark} />
 
 			<p> This is the text in the app component</p>
 		</div>
