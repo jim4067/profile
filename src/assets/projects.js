0618740f36f76d62e8fb7eb60cf4c70f48576da1
jim4067 <jimmyimpulse2@gmail.com>
2021-02-02 02:10:48 +0300

    feat: changed my resume

---
+++ src/assets/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+    {
+        name: "project1",
+        id: 121,
+        description: "this is the description of the project that will be",
+        link: 'https://bla-bla-bla'
+    }
+];
+
+export default projects;
