e08c08e1562d4af0d2f0260c8a590773f29f9f0f
jim4067 <jimmyimpulse2@gmail.com>
2021-02-28 21:44:37 +0300

    feat: the projects section is done -> will continue in near future when classwork reduces

---
+++ src/assets/projects-list.js
@@ -0,0 +1,55 @@
+const projects = [
+    // {
+    //     name: "test-project1",
+    //     id: 200,
+    //     description: "this is the description of the project that will be",
+    //     live_link: 'https://bla-bla-bla',
+    //     github_link: 'https://github.com/jim4067/demo-projo'
+    // },
+    // {
+    //     name: "test-project2",
+    //     id: 300,
+    //     description: "this is the description of the project that will be",
+    //     live_link: 'https://bla-bla-bla',
+    //     github_link: 'https://github.com/jim4067/demo-projo'
+    // },
+    {
+        id: 1,
+        name: "Blogit",
+        tech_stack: "Node + React + Express + MongoDB",
+        description: "A web app that allows the user to save blogs he/she comes across over the internet.",
+        live_link: "http://jims-blogit.herokuapp.com/",
+        github_link: "https://github.com/jim4067/blogit",
+        case_study_page: "src/pages....",
+    },
+    {
+        id: 2,
+        name: "Gh-pr",
+        tech_stack: "React ",
+        description: "A simple web application that uses the Github api to search for pull requests made on public repositories.",
+        live_link: "",
+        github_link: 'https://github.com/jim4067/gh-pr',
+        case_study_page: "src/pages....",
+    },
+    {
+        id: 3,
+        name: "Developer-anecdotes",
+        tech_stack: "Rust + GTK",
+        description: "A simple desktop app that display random developer anecdotes.",
+        live_link: "",
+        github_link:"https://github.com/jim4067/developer-anecdotes.",
+        case_study_page: "src/pages....",
+    },
+    {
+        id: 4,
+        name: "Shalle",
+        description: "A simple website I build for a friend and deployed to netlify.",
+        tech_stack: "React + react-router",
+        live_link: 'shalle.netlify.app',
+        github_link: 'https://github.com/jim4067/shalle',
+        case_study_page: "src/pages....",
+        in_depth: "I built this website for a friend learning about rect-router and deployed it to netlify.",
+    },
+];
+
+export default projects;
---
+++ src/components/Projects.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import projects from '../assets/projects-list';
+
+const Wrapper = styled.div`
+    margin-top: 3vh;
+`;
+
+const Heading = styled.h2`
+    margin: 3vh 0;
+`;
+
+const ProjectContainer = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const LinkWrap = styled.a`
+    color: ${({ dark }) => dark ? "#eceff4" : "#4c566a"};
+    text-decoration: none;
+    transition: .2s linear;
+
+    :hover{
+        cursor: pointer;
+        transform: scale(1.08);
+    }
+`;
+
+const SingleProject = styled.div`
+    /* background-color: ${({ dark }) => dark ? "#353848" : "#eceff4"}; */
+    border: ${({ dark }) => dark ? "1px solid #48dbfb" : "none"};
+    border-radius: 4px;
+    box-shadow: ${({ dark }) => dark ? "0px 5px 5px -5px darkslategrey" : "0px 7px 15px -5px  #00000070"};
+    margin: 1vh 30px 1vh 0vw;
+    outline: none;
+    padding: 4px 20px;
+    width: 43.7vw;
+
+    @media (max-width: 1200px) {
+        width: 85vw;
+        margin-right: 30px;
+    }
+`;
+
+const ProjectName = styled.h4`
+    text-decoration: solid underline #48dbfb ;
+    line-height: 1;
+`;
+
+const TechStack = styled.p`
+    line-height: 2;
+`;
+
+const ProjectDescription = styled.p`
+    opacity: .9;
+`;
+
+const Projects = ({ dark }) => {
+
+    return (
+        <Wrapper>
+
+            <hr style={{ "margin-right": "30px" }} /> {/* A line separator*/}
+            <Heading>Projects Show & Tell</Heading>
+
+            <ProjectContainer>
+                {
+                    projects.map((proj) =>
+                        <LinkWrap dark={dark} href={proj.github_link}>
+                            <SingleProject dark={dark} >
+                                <ProjectName>{proj.name}</ProjectName>
+                                <TechStack>{proj.tech_stack}</TechStack>
+                                <ProjectDescription>{proj.description}</ProjectDescription>
+                            </SingleProject>
+                        </LinkWrap>
+                    )
+                }
+            </ProjectContainer>
+        </Wrapper>
+    );
+}
+
+export default Projects;
