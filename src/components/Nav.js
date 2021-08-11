3abc7d3d675694b9f0a3770be38f4a2dbda19091
jim4067 <jimmyimpulse2@gmail.com>
2021-08-11 20:21:41 +0300

    :tada: :tada:

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Nav = ({ dark }) => {
+
+    return (
+        <header>
+            <nav>
+                {/* logo here */}
+                {/* theme handler */}
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
