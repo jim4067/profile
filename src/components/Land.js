19834157da8b7c32f54874b5b0e6c868ef7acb05
jim4067 <jimmyimpulse2@gmail.com>
2021-08-12 22:15:46 +0300

    :tada: the testing landing page:tada:

---
+++ src/components/Land.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const Land = ({ dark }) => {
+
+    return (
+        <div>
+            this is the landing page
+        </div>
+    )
+}
+
+export default Land;
